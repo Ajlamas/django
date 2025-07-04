8a91dccf6198c0bbd3fb3cf0ee7fa8236ddc028d
Ajlamas <alberto.l.lage@gmail.com>
2025-07-04 12:27:03 +0200

    actualizando el wsgi.py para señalar la ruta correcta de manage.py

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -6,11 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
+import sys
 import os
 
-from django.core.wsgi import get_wsgi_application
+path = '/home/allamas/django/myproject'
+if path not in sys.path:
+    sys.path.append(path)
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
